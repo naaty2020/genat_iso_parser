c2feecca43d7b5a142464c19a1e49851a3af2c0a
Nathnael Fikru <nattyf2008@gmail.com>
2024-05-18 10:08:10 +0300

    Second Commit

---
+++ src/genat_iso_parser/exceptions.py
@@ -0,0 +1,20 @@
+class LengthError(Exception):
+    def __init__(self, errors, message='Invalid Field Length.'):
+        super().__init__(message)
+        self.errors = errors
+
+class InvalidBitmap(Exception):
+    def __init__(self, errors, message='Invalid Bitmap.'):
+        super().__init__(message)
+        self.errors = errors
+
+class PadValueError(ValueError):
+    def __init__(self, errors, message='Non Integer Field Length.'):
+        super().__init__(message)
+        self.errors = errors
+
+class UnsupportedIsoVersion(KeyError):
+    def __init__(self, errors):
+        message = f'Unsupported Iso version {errors["version"]}.'
+        super().__init__(message)
+        self.errors = errors
